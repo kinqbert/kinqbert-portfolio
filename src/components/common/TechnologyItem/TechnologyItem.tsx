ad11a3d3cd288ecc50206eb6fe615626d885dac5
kinqbert <perehodko1010@gmail.com>
2025-05-03 01:53:27 +0300

    added rotate animation for technology item

--- src/components/common/TechnologyItem/TechnologyItem.tsx
+++ src/components/common/TechnologyItem/TechnologyItem.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 import { Technology } from "@/types/Technology";
-
 import styles from "./TechnologyItem.module.scss";
 
 interface Props {
@@ -10,11 +12,31 @@ interface Props {
 }
 
 export const TechnologyItem = ({ technology, variant = "large" }: Props) => {
+  const [rotate, setRotate] = useState(0);
+
+  const handleMouseEnter = () => {
+    if (variant === "small") return;
+
+    const min = -5;
+    const max = 5;
+    const angle = Math.floor(Math.random() * (max - min + 1)) + min;
+    setRotate(angle);
+  };
+
+  const handleMouseLeave = () => {
+    setRotate(0);
+  };
+
   return (
     <div
       className={`${styles.technologyItem} ${
         styles[`technologyItem--${variant}`]
       }`}
+      style={
+        variant === "large" ? { transform: `rotate(${rotate}deg)` } : undefined
+      }
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <Image
         className={`${styles.image} ${styles[`image--${variant}`]}`}
